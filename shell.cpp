6d97d2ea98b37002d97cfc4fa31bf7c9b94aa4dd
Richard Young <ryoung141@gmail.com>
2017-04-18 21:13:55 -0700

    compiles, conversion from string to cstring complete for parser, ready for push

--- shell.cpp
+++ shell.cpp
@@ -2,16 +2,34 @@
 // CS170 SPRING 2017
 // C++ implementation of a basic UNIX shell
 // shell.cpp
+#include <iostream>
+#include <cstdio> 
+#include <cstdlib>
+#include <string> //getline
+#include <cstring>
 
-char *parse(){
-	//TODO: implement...
-	size_t buf = 0;
-	char *line;
-	
+int execute(char **args){
+	return 0;
 }
 
-void tokenize(char *line){
+char *parse(){
+	//TODO: test?
+	size_t buf = 0;
+	std::string read;
+	std::getline(std::cin, read); //thank you c++ gods for making this function
+	char *read_c = new char[read.length() + 1];
+	std::strcpy (read_c, read.c_str());
+	
+	return read_c;
+}
+
+char **tokenize(char *line){
 	//TODO: implement
+	char *token;
+	char **tokens;
+	char delimeter[] = " \t;\n";
+	
+	return tokens;
 }
 
 void shell_loop(){
@@ -24,7 +42,7 @@ void shell_loop(){
 	do{
 		//idk... because that's what shells do for some reason..\
 		looks fresh
-		cout << "> ";
+		std::cout << "> ";
 		
 		//read line
 		read_line = parse();
@@ -35,7 +53,7 @@ void shell_loop(){
 		//execute command
 		running = execute(split_args);
 		
-	}while(running)
+	}while(running);
 }
 
 int main(int argc, char **argv){
@@ -43,5 +61,5 @@ int main(int argc, char **argv){
 	//run command loop
 	shell_loop();
 	
-	return EXIT_SUCCESS
+	return EXIT_SUCCESS;
 }
