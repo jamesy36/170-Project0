9180b8c6443b213fb8a46a2582dc65a20d0203f8
Richard Young <richard_young@umail.ucsb.edu>
2017-04-21 11:02:37 -0700

    I/O redirection working, piping might work(?)

--- shell.c
+++ shell.c
@@ -25,49 +25,145 @@ void handler_function(int parameter){
 	}
 }
 
-void launch(char** args){
-	int i = 0;
-	char in_key[] = "<";
-	char out_key[] = ">";
-	int meta_in = 0;
-	int meta_out = 0;
-	int input_i = 0;
-	int output_i = 0;
-	while (args[i] != NULL){
-		if(strcmp(args[i], in_key)){
-			meta_in = 1;
-			input_i = i + 1;
-		}
-		if(strcmp(args[i], out_key)){
-			meta_out = 1;
-			output_i = i + 1;
-		}
-		i++;
-	}
-	
-	pid_t pid;
-	if ((pid = fork()) < 0){
-		printf("ERROR FORKING\n");
-		exit(EXIT_FAILURE);
-	}
-	else if(pid){
-		waitpid(pid, NULL, 0);
-	}
-	else{
-		if (meta_in){
-			int fd0 = open(args[input_i], O_RDONLY);
-			dup2(fd0, STDIN_FILENO);
-			close(fd0);
-		}
-		if (meta_out){
-			int fd1 = creat(args[output_i], 0644);
-			dup2(fd1, STDOUT_FILENO);
-			close(fd1);
-		}
-		execvp(*args, args);
-	}
+int find_arg(char** args, char* command){
+  int found = -1;
+  int i = 0;
+  while(args[i] != NULL){
+    if(strcmp(command, args[i])){
+      ++i;
+    }
+    else{
+      found = i;
+      break;
+    }
+  }
+  //testing
+  //  printf("find_arg success\n");
+  return found;
 }
 
+  void split_arg(char** args, char** split_args, int split_point){
+    int i = 0;
+    for (i; i < split_point; ++i){
+      split_args[i] = args[i];
+    }
+    split_args[i] = '\0';
+    //testing
+    // printf("split success\n");
+    // int temp;
+    //    for (temp = 0; temp < i; temp++){
+    // printf("%s\n", split_args[temp]);
+    // }
+  }
+
+  void launch(char** args){
+    signal (SIGTSTP, handler_function);
+    int i = 0;
+    pid_t pid;
+    if (find_arg(args, ">")!= -1){
+      //test
+      // printf("'>' found\n");
+      
+      int pos = find_arg(args, ">");
+      char* f = args[pos+1];
+      char* new_arg[pos];
+      split_arg(args, new_arg, pos);
+      
+      pid = fork();
+      if(pid == 0){ //child
+	int out = open(f, O_CREAT | O_WRONLY, S_IRWXU);
+	dup2(out, STDOUT_FILENO);
+	close(out);
+	
+	execvp(*new_arg, new_arg);
+	exit(EXIT_FAILURE);
+      }
+      else{
+	wait(0);
+      }
+    }
+    else if(find_arg(args, "<") != -1){
+      //test
+      // printf("'<' found\n");
+      
+      int pos = find_arg(args, "<");
+      char* f = args[pos+1];
+      char* new_arg[pos];
+      split_arg(args, new_arg, pos);
+      
+      pid = fork();
+      if(pid == 0){//child
+	int in = open(f, O_RDONLY, S_IRWXU);
+	if (in == -1){
+	  printf("%s not found in current directory \n", f);
+	}
+	else{
+	  dup2(in, STDIN_FILENO);
+	  close(in);
+
+	  execvp(args[0], new_arg);
+	  exit(EXIT_FAILURE);
+	}
+      }
+      else{
+	wait(0);
+      }
+    }
+    else if (find_arg(args, "|") != -1){
+      int pos = find_arg(args, "|");
+      int input = STDIN_FILENO;
+      int output = STDOUT_FILENO;
+
+      int t[2];
+      pipe(t);
+      output = t[1];
+
+      args[pos] = '\0';
+      pid_t pid1 = fork();
+      if(pid1 == 0){//child
+	if (input != STDIN_FILENO){
+	  dup2(input, STDIN_FILENO);
+	}
+	if(output != STDOUT_FILENO){
+	  dup2(output, STDOUT_FILENO);
+	}
+	execvp(*args, args);
+	exit(EXIT_FAILURE);
+      }
+      else{
+	wait(0);
+      }
+      close(t[1]);
+
+      input = t[0];
+      output = STDOUT_FILENO;
+
+      pid_t pid2 = fork();
+      if(pid2 == 0){//child
+	if(input != STDIN_FILENO){
+	  dup2(input, STDIN_FILENO);
+	}
+	if(output!=STDOUT_FILENO){
+	  dup2(output, STDOUT_FILENO);
+	}
+	execvp((args + pos + 1)[0], args + pos + 1);
+	exit(EXIT_FAILURE);
+      }
+      else{
+	wait(0);
+      }
+    }
+    else{
+      pid = fork();
+      if(pid){
+	waitpid(pid, NULL, 0);
+      }
+      else{
+	execvp(*args, args);
+      }
+    }
+  }
+  
 int execute(char **args_list){
 	signal (SIGTSTP, handler_function);
 	//1 means keep looping, 0 means stop execution
@@ -142,7 +238,7 @@ void shell_loop(){
 	do{
 		//idk... because that's what shells do for some reason.. 
 		//looks fresh I guess
-	  printf("> ");
+	  //  printf("shell> ");
 		
 		//read line
 		read_line = parse();
